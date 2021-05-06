5e6a2cd155ca7bbbb9ff087d2c925879a61423b4
Isabella <isasanchis3@gmail.com>
2021-05-06 17:24:39 -0300

    updates

--- citacoes/citacoes/src/components/Citacao.vue
+++ citacoes/citacoes/src/components/Citacao.vue
@@ -1,12 +1,14 @@
 <template>
     <div class="citacao">
+        <slot name="autor"></slot>
         <slot></slot>
+        <div class="autor"><slot name="fonte"></slot></div>
     </div>
 </template>
 
 <script>
 export default {
-    props: ['texto']
+    
 }
 </script>
 
@@ -16,5 +18,9 @@ export default {
         background-color: rgba(0, 0, 0, .1);
         padding: 25px;
         max-width: 450px;
+    }
+
+    .autor {
+        float: right;
     }
 </style>
--- citacoes/citacoes/src/components/Citacoes.vue
+++ citacoes/citacoes/src/components/Citacoes.vue
@@ -5,9 +5,9 @@
             <button @click="numero++">&gt;</button>
         </span>
         <Citacao>
-            <h1>{{ citacoes[indice].autor }}</h1>
+            <h1 slot="autor">{{ citacoes[indice].autor }}</h1>
             <p>{{ citacoes[indice].texto }}</p>
-            <h6>{{ citacoes[indice].fonte }}</h6>
+            <h6 slot="fonte">{{ citacoes[indice].fonte }}</h6>
         </Citacao>
     </div>
 </template>
